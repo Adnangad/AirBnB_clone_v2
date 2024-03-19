b5c1a6346cd2e17be688195acb4ab5154f92f19f
Adnangad <gardobuyaadnan@gmail.com>
2024-03-19 01:33:06 -0700

    10-amenity and many to many relationships

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationships
+from sqlalchemy import String, ForeignKey, Column
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from model.place import place_amenity
 
-
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary=place_amenity)
--- models/place.py
+++ models/place.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
 
+place_amenity = Table(
+        'place_amenity',
+        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+        Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """Contains class place."""
@@ -23,6 +27,11 @@ class Place(BaseModel, Base):
     amenity_ids = []
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", cascade="all, delete, delete-orphan", backref="place")
+        amenities = relationship(
+                'Amenity',
+                secondary=place_amenity,
+                back_populates='place_amenity',
+                viewonly=False)
     else:
         @property
         def review(self):
@@ -32,3 +41,13 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     ls.append(review)
             return ls
+        @property
+        def amenities(self):
+            ls = []
+            ls.append(self.amenity_ids)
+            return ls
+        
+        @amenities.setter
+        def amenities(self, obj=None):
+            if isinstance(obj, Amenity) and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
