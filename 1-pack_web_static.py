029624231fbc2908ecf8a89f4889a6e6ee5108cd
Adnangad <gardobuyaadnan@gmail.com>
2024-04-03 23:49:41 -0700

    1-compressed folder

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module generates a.tgz archive."""
+from fabric.api import *
+from datetime import datetime
+
+@task
+def do_pack():
+    fold = 'web_static'
+    arch = f'web_static_{datetime.now().strftime("%Y%m%d%H%M%S")}.tgz'
+    local('mkdir -p versions')
+    fin = local(f'tar -cvzf versions/{arch} {fold}')
+    if fin.succeeded:
+        return f'versions/{arch}'
+    else:
+        return None
