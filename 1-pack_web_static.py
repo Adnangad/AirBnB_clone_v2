b82f74c8af2318969bc8e001ef9fa266c4b081fa
Adnangad <gardobuyaadnan@gmail.com>
2024-04-04 00:05:22 -0700

    1-compressed folder

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,12 +8,10 @@ from datetime import datetime
 
 def do_pack():
     """Compresses a folder to a .tgz archive"""
-
-    fold = 'web_static'
-    arch = f'web_static_{datetime.now().strftime("%Y%m%d%H%M%S")}.tgz'
-    local('mkdir -p versions')
-    fin = local(f'tar -cvzf versions/{arch} {fold}')
-    if fin.succeeded:
-        return f'versions/{arch}'
+    local("sudo mkdir -p versions")
+    archive = f"versions/web_static_{datetime.now().strftime('%Y%m%d%H%M%S')}.tgz"
+    fin = local(f"sudo tar-cvzf {archive} web_static")
+    if fin.succeed:
+        return archive
     else:
         return None
