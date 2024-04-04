b5fbe4af88356e19cfe20bfec2ebce6e73a81235
Adnangad <gardobuyaadnan@gmail.com>
2024-04-04 03:07:57 -0700

    2-deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" compresses a folder to an archive """
+"""deploys the archive into a server """
 
 
 from fabric.api import *
@@ -9,25 +9,26 @@ from os.path import exists
 
 env.hosts = ['54.87.172.130', '52.204.105.20']
 
+
 def do_deploy(archive_path):
+    """ """
     if exists(archive_path) is False:
         return False
-    remote_path = '/tmp/'
-    final_path = '/data/web_static/releases' + f"{archive_path.split('.')[0]}"
-    archive = archive_path.split('/')[-1]
-    pat = remote_path + archive
-    env.hosts = ['54.87.172.130', '52.204.105.20']
-    try:
-        put(archive_path, remote=remote_path)
-        run(f'mkdir -p {final_path}')
-        run(f'tar -xzf {pat} -C {final_path}')
-        run(f"rm {pat}")
-        run("mv {}/web_static/* {}/".format(final_path, final_path))
-        run("rm -rf {}/web_static".format(final_path))
-        lnk = '/data/web_static/current'
-        run(f'rm {lnk}')
-        run(f'ln -s {final_path} {lnk}')
-        return True
-    except Exception:
-        return False
+    arch = archive_path.split('/')[-1]
+    arch_no_extension = '/data/web_static/releases/' + "{}".format(arch.split('.')[0])
+    pat = "/tmp/" + arch
+    remote_path = "/tmp/"
 
+    try:
+        put(archive_path, remote_path)
+        run("mkdir -p {}/".format(arch_no_extension))
+        run("tar -xzf {} -C {}/".format(pat, arch_no_extension))
+        run("rm {}".format(pat))
+        run("mv {}/web_static/* {}/".format(arch_no_extension, arch_no_extension))
+        run("rm -rf {}/web_static".format(arch_no_extension))
+        lnk = "/data/web_static/current"
+        run(f"rm -rf {lnk}")
+        run("ln -s {}/ {}".format(no_tgz, lnk))
+        return True
+    except:
+        return False
