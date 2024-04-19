3544bc24acf20bb4a7a56e7cdfb0f00581cb448d
Adnangad <gardobuyaadnan@gmail.com>
2024-04-19 01:11:40 -0700

    0-displaying a web app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def disp():
+    """This returns a specific message."""
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+run(app, host='0.0.0.0', port=5000)
