ce975720bd9c268f1fabebdbb3bc3d05305f7212
Adnangad <gardobuyaadnan@gmail.com>
2024-04-19 01:33:45 -0700

    2-display web app with args

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""starts a web app application and displays message on a web app."""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def disp():
+    """This returns a specific message."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def disp_path():
+    """This returns a specific message."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def disp_args(text):
+    """This returns a specific message."""
+    if "_" in text:
+        text.replace("_", "")
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
