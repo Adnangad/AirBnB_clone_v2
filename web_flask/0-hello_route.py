3f36f9f02088b3acb7b1cb9df802c0e2cd3474d6
Adnangad <gardobuyaadnan@gmail.com>
2024-04-19 01:03:06 -0700

    0-displaying a web app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,11 +2,14 @@
 from flask import Flask
 """This module displays a message on the webpage"""
 
+
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def disp():
-  return "Hello HBNB!"
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
