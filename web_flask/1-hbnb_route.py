1dd49e5c89d67fdf714d464005983731b73d1d68
Adnangad <gardobuyaadnan@gmail.com>
2024-04-19 01:25:35 -0700

    1-web app with path

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module starts a web app application and displays message on a web app."""
+"""starts a web app application and displays message on a web app."""
 from flask import Flask
 
 
@@ -11,10 +11,12 @@ def disp():
     """This returns a specific message."""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def disp_path():
     """This returns a specific message."""
-    return "HBNB!"
+    return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
